ead70d3de90d934f1df8b953f64b8a53994a237b
ak-nagae <ak-nagae@kddi-agile.com>
2025-12-27 13:51:20 +0900

    jjの設定入れた

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 挿入モードでjjを押すとノーマルモードに戻る
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
