1e24c4e822872c43db794076280f17397cf258f9
ak-nagae <ak-nagae@kddi-agile.com>
2025-12-27 10:07:46 +0900

    nvim Initial setting

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,42 @@
+return {
+  -- Catppuccin theme configuration
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = true, -- disables setting the background color
+      show_end_of_buffer = false,
+      term_colors = true,
+      dim_inactive = {
+        enabled = false,
+      },
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        mason = true,
+        noice = true,
+        notify = true,
+        which_key = true,
+        mini = {
+          enabled = true,
+        },
+      },
+    },
+  },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
